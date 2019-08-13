6480b2fc5575b484256f75fa1faaa2e4ad58fa82
Felix Oduro-Fosu <felix@iscientia.com>
2019-08-13 10:32:43 +0200

    global app CTRL 1.getting inout

--- app.js
+++ app.js
@@ -10,15 +10,27 @@ var budgetController = (function (params) {
 //UI CONTROLLER
 var UIController = (function () {
     
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
     return function () {
         getInput: function() {
             return {
-                type: document.querySelector('.add__type').value, // will be either inc or exp.
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp.
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             };
         }
     };
+
+    //making this object public so that other methods can access it.
+    getDOMstrings: function() {
+        return DOMstrings;
+    }
 
 })();
 
@@ -28,9 +40,15 @@ var UIController = (function () {
 //how to connect the two controllers.
 var controller = (function (budgetCtrl, UICtrl) {
 
+    //initializing the object...
+    var DOM = getDOMstrings.DOMstrings();
+
     var ctrlAddItem = function () {
         
         //1. get field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
 
         //2. add the item to the budget controller
 
@@ -42,7 +60,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13) {
