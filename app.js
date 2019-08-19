147b9c4487f39eee480e8c5e2fc1bebf8c6bb21a
Felix Oduro-Fosu <felix@iscientia.com>
2019-08-19 09:04:42 +0200

    input Ctrl

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 //using of closure and IIFE to keep the controller private 
 var budgetController = (function (params) {
 
-    
+
 })();
 
 
 
 //UI CONTROLLER
 var UIController = (function () {
-    
+
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
@@ -17,20 +17,21 @@ var UIController = (function () {
         inputBtn: '.add__btn'
     }
 
-    return function () {
-        getInput: function() {
+    return {
+        getInput: function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp.
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
-        };
-    };
+        },
 
-    //making this object public so that other methods can access it.
-    getDOMstrings: function() {
-        return DOMstrings;
-    }
+        //making this object public so that other methods can access it.
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+
+    };
 
 })();
 
@@ -41,10 +42,10 @@ var UIController = (function () {
 var controller = (function (budgetCtrl, UICtrl) {
 
     //initializing the object...
-    var DOM = getDOMstrings.DOMstrings();
+    var DOM = UICtrl.getDOMstrings();
 
     var ctrlAddItem = function () {
-        
+
         //1. get field input data
         var input = UICtrl.getInput();
         console.log(input);
@@ -61,12 +62,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     }
 
     document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-    
-    document.addEventListener('keypress', function(event) {
+
+    document.addEventListener('keypress', function (event) {
         if (event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
         }
-        
+
     })
-    
+
 })(budgetController, UIController);
